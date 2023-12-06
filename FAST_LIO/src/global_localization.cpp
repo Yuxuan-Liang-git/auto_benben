e07a3db02ee8454dcc9b425cfa2c51f4b9d1a547
Yuxuan-Liang-git <839003509@qq.com>
2023-12-06 14:36:42 +0800

    尝试把过滤后的点云话题发布显示，后续是得到全局定位信息，更新map2odom

--- FAST_LIO/src/global_localization.cpp
+++ FAST_LIO/src/global_localization.cpp
@@ -2,7 +2,8 @@
 
 GlobalLocalization::GlobalLocalization(): Node("localization_node")
 {
-    
+    odom_received_ = false;
+    global_pc_received_ = false;
     // 创建发布者
     pub_submap = this->create_publisher<sensor_msgs::msg::PointCloud2>("/submap", 1);
     pub_map_to_odom = this->create_publisher<nav_msgs::msg::Odometry>("/map_to_odom", 1);
@@ -14,19 +15,58 @@ GlobalLocalization::GlobalLocalization(): Node("localization_node")
     sub_odometry = this->create_subscription<nav_msgs::msg::Odometry>(
         "/Odometry", 1, std::bind(&GlobalLocalization::cb_save_cur_odom, this, std::placeholders::_1));
     RCLCPP_INFO(this->get_logger(), "Create pub&sub Succeed");
-
     this->global_pc_init();
+    timer = this->create_wall_timer(
+        std::chrono::milliseconds(static_cast<int>(1000 / FREQ_LOCALIZATION)),
+        std::bind(&GlobalLocalization::cb_locate_pos, this));
 
 }
 GlobalLocalization::~GlobalLocalization()
 {
 }
 
-    // 回调函数
+// 滤去可视范围外保存的点云
+void GlobalLocalization::get_global_map_in_FOV()
+{
+    // 将保存的点云转到lidar坐标系下
+    pcl::PointCloud<pcl::PointNormal>::Ptr temp_pc;
+    pcl::transformPointCloud (*global_pc, *temp_pc, T_map_to_odom);
+
+	//创建条件限定下的滤波器
+	pcl::ConditionAnd<pcl::PointNormal>::Ptr range_cond(new pcl::ConditionAnd<pcl::PointNormal>());//创建条件定义对象range_cond
+	//为条件定义对象添加比较算子
+	range_cond->addComparison(pcl::FieldComparison<pcl::PointNormal>::ConstPtr(new
+		pcl::FieldComparison<pcl::PointNormal>("x", pcl::ComparisonOps::GT, -FOV_FAR)));//添加在x字段上大于 -.1 的比较算子
+	range_cond->addComparison(pcl::FieldComparison<pcl::PointNormal>::ConstPtr(new
+		pcl::FieldComparison<pcl::PointNormal>("x", pcl::ComparisonOps::LT, FOV_FAR)));//添加在x字段上小于 1.0 的比较算子
+	range_cond->addComparison(pcl::FieldComparison<pcl::PointNormal>::ConstPtr(new
+		pcl::FieldComparison<pcl::PointNormal>("y", pcl::ComparisonOps::GT, -FOV_FAR)));//添加在y字段上大于 -.1 的比较算子
+	range_cond->addComparison(pcl::FieldComparison<pcl::PointNormal>::ConstPtr(new
+		pcl::FieldComparison<pcl::PointNormal>("y", pcl::ComparisonOps::LT, FOV_FAR)));//添加在y字段上小于 1.0 的比较算子
+
+	pcl::ConditionalRemoval<pcl::PointNormal> cr;	//创建滤波器对象
+	cr.setCondition(range_cond);			//用条件定义对象初始化
+	cr.setInputCloud(temp_pc);			//设置待滤波点云
+	cr.setKeepOrganized(false);			//保持点云结构，即有序点云经过滤波后，仍能够保持有序性。(是否保留无效点云数据的位置信息)
+	
+	cr.filter(*global_pc_in_FOV);				//执行滤波，保存滤波结果于cloud_filtered
+
+    sensor_msgs::msg::PointCloud2 laserCloudmsg;
+    pcl::toROSMsg(*global_pc_in_FOV, laserCloudmsg);
+    laserCloudmsg.header.stamp = this->get_clock()->now();
+    laserCloudmsg.header.frame_id = "body";
+
+    pub_submap->publish(laserCloudmsg)
+}
+   
+// 回调函数
 void GlobalLocalization::cb_save_cur_scan(const sensor_msgs::msg::PointCloud2::SharedPtr pc_msg) {
     // 处理接收到的PointCloud2消息
     // 将ROS的PointCloud2转换为PCL的PointCloud并存储在类内
     // RCLCPP_INFO(this->get_logger(), "PointCloud Received!!");   
+    if(!global_pc_received_){
+        global_pc_received_ = true;
+    }
     pcl::PointCloud<pcl::PointNormal>::Ptr cur_scan_pc (new pcl::PointCloud<pcl::PointNormal>);
     pcl::fromROSMsg(*pc_msg, *cur_scan_pc);
 
@@ -35,9 +75,24 @@ void GlobalLocalization::cb_save_cur_scan(const sensor_msgs::msg::PointCloud2::S
 void GlobalLocalization::cb_save_cur_odom(const nav_msgs::msg::Odometry::SharedPtr msg) {
     // 处理接收到的Odometry消息
     RCLCPP_INFO(this->get_logger(),"Odometry received!");
+    if(!odom_received_){
+        odom_received_ = true;
+    }
     T_map_to_odom.translation() << 
         msg->pose.pose.position.x,msg->pose.pose.position.y,msg->pose.pose.position.z;
 
+    Eigen::Quaterniond quat
+                (msg->pose.pose.orientation.x,msg->pose.pose.orientation.y,
+                    msg->pose.pose.orientation.z,msg->pose.pose.orientation.w);
+    T_map_to_odom.rotate(quat);
+}
+
+void GlobalLocalization::cb_locate_pos()
+{
+    if(odom_received_&global_pc_received_){
+        RCLCPP_INFO(this->get_logger(),"Global localization by scan-to-map matching......");
+        get_global_map_in_FOV();
+    }
 }
 
 void GlobalLocalization::global_pc_init()
--- FAST_LIO/src/global_localization.h
+++ FAST_LIO/src/global_localization.h
@@ -6,18 +6,19 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/common/transforms.h>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/conditional_removal.h>
 #include <iostream>
 #include <string>
 
 using namespace std;
-
-const  float MAP_VOXEL_SIZE = 0.4;
+const float MAP_VOXEL_SIZE = 0.4;
 #define SCAN_VOXEL_SIZE = 0.1;
-#define FREQ_LOCALIZATION = 0.5;
+const float FREQ_LOCALIZATION = 0.5;
 #define LOCALIZATION_TH = 0.93;
 #define FOV = 6.28319;  // 360 degrees in radians
-#define FOV_FAR = 70.0;
+const float FOV_FAR = 70.0;
 
 class GlobalLocalization: public rclcpp::Node
 {
@@ -30,10 +31,19 @@ class GlobalLocalization: public rclcpp::Node
 private:
     void cb_save_cur_scan(const sensor_msgs::msg::PointCloud2::SharedPtr pc_msg);
     void cb_save_cur_odom(const nav_msgs::msg::Odometry::SharedPtr msg);
+    void cb_locate_pos();
     void global_pc_init();
+    void get_global_map_in_FOV();
+    void locate();
+
+    bool odom_received_;
+    bool global_pc_received_;
+    
     pcl::PointCloud<pcl::PointNormal>::Ptr cur_scan_pc;
     pcl::PointCloud<pcl::PointNormal>::Ptr global_pc;
-    Eigen::Affine3f T_map_to_odom = Eigen::Affine3f::Identity();
+    pcl::PointCloud<pcl::PointNormal>::Ptr global_pc_in_FOV;
+    Eigen::Affine3d T_map_to_odom = Eigen::Affine3d::Identity();
+    rclcpp::TimerBase::SharedPtr timer;
 
     // 发布者
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pub_submap;
